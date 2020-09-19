bea88db9ce9e1d5fd637e6886bbb7f2d204dce13
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2020-09-19 20:20:14 +0200

    Clean the pull and install script.

--- scripts/pull_and_compile.py
+++ scripts/pull_and_compile.py
@@ -14,7 +14,7 @@ def run_command( cmd, where ):
 
 def get_modules():
 	modules = []
-	regex = (ur"\<module\>(.+)\<\/module\>")
+	regex = (u"\<module\>(.+)\<\/module\>")
 	with open( PATH_TO_POM_FILE, 'r' ) as pom_file:
 		matches = re.finditer(regex, pom_file.read(), re.MULTILINE)
 		for matchNum, match in enumerate(matches, start=1):
@@ -25,34 +25,34 @@ def check_if_clean( module ):
 	print( '\033[1mChecking module %s \033[0m' % module )
 	path = os.path.join( ROOT_PATH, module )
 	out = run_command( "git status -uno", path )
-	
+
 	# Get branch name.
-	branch_match = re.search( ur"^On branch (.+)$", out, re.MULTILINE )
+	branch_match = re.search( u"^On branch (.+)$", out, re.MULTILINE )
 	print( 'Current branch: %s' % branch_match.group( 1 ) )
-	
+
 	# Check if we are up to date.
-	up_to_date = re.search( ur"Your branch is up to date with", out, re.MULTILINE )
+	up_to_date = re.search( u"Your branch is up to date with", out, re.MULTILINE )
 	if not up_to_date:
 		print( 'Repo not up to date with remote. Aborting. ' )
 		return False
 
 	# Check if there are unstaged commits.
-	unstaged = re.search( ur"Changes not staged for commit:", out, re.MULTILINE )
+	unstaged = re.search( u"Changes not staged for commit:", out, re.MULTILINE )
 	if unstaged:
 		print( 'There are unstaged changes. Aborting. ' )
 		return False
-	
+
 	print( 'All good.' )
 	return True
 
 def git_pull( module ):
-        print( '\033[1mPulling in  module %s \033[0m' % module )                    
-        path = os.path.join( ROOT_PATH, module )                                 
-        out = run_command( "git pull", path )  	
+        print( '\033[1mPulling in  module %s \033[0m' % module )
+        path = os.path.join( ROOT_PATH, module )
+        out = run_command( "git pull", path )
 
 def install():
-        cmd = "mvn clean install"  	
-	subprocess.call( cmd, cwd = ROOT_PATH, shell = True ) 
+    cmd = "mvn clean install"
+    subprocess.call( cmd, cwd = ROOT_PATH, shell = True )
 
 #-----------------------
 # MAIN
@@ -60,27 +60,27 @@ def install():
 
 def main():
 
-	modules = get_modules()
+    modules = get_modules()
 
-	# Check each module.
-	print( '\n----------------' )
-	print( 'Checking modules' )
-	for module in modules:
-		if not check_if_clean( module ):
-			return 
-	
-	# Pull each module.
-	print( '\n-------------------' )
-	print( 'Pulling from remote' )
-	for module in modules:
-		git_pull( module )	
-	print( 'Done.' )
-	
+    # Check each module.
+    print( '\n----------------' )
+    print( 'Checking modules' )
+    for module in modules:
+        if not check_if_clean( module ):
+            return
 
-	print( '\n----------' )
-	print( 'Installing' )
-	install()
-	print( 'Done.' )
+    # Pull each module.
+    print( '\n-------------------' )
+    print( 'Pulling from remote' )
+    for module in modules:
+        git_pull( module )
+    print( 'Done.' )
+
+
+    print( '\n----------' )
+    print( 'Installing' )
+    install()
+    print( 'Done.' )
 
 if __name__ == "__main__":
     main()

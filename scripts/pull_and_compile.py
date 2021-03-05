cc762bb0bdc0b97540f17971e605717d7a7c7d97
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2021-03-05 19:06:57 +0100

    Make the deploy script works on Windows OS and accept tag branches.

--- scripts/pull_and_compile.py
+++ scripts/pull_and_compile.py
@@ -10,8 +10,11 @@ PATH_TO_POM_FILE = os.path.join( ROOT_PATH, 'pom.xml' )
 
 
 def run_command( cmd, where ):
-	return  subprocess.check_output( cmd, cwd = where, shell = True)
-
+	print( 'Running %s on %s' % ( cmd, where ) )
+	try:
+		return subprocess.check_output( cmd, cwd = where, shell = True)
+	except subprocess.CalledProcessError as e:
+		print( 'Could not run %s on %s' % ( cmd, where ) )
 
 def get_modules():
 	modules = []
@@ -23,20 +26,25 @@ def get_modules():
 	return modules
 
 def check_if_clean( module, skip_up_to_date = False ):
-	print( '\033[1mChecking module %s \033[0m' % module )
+	print( 'Checking module %s ' % module )
 	path = os.path.join( ROOT_PATH, module )
-	out = run_command( "git status -uno", path )
+	out = run_command( "git status -uno", path ).decode('ISO-8859-1')
 
 	# Get branch name.
 	branch_match = re.search( u"^On branch (.+)$", out, re.MULTILINE )
-	print( 'Current branch: %s' % branch_match.group( 1 ) )
+	if branch_match is not None:
+		print( 'Current branch: %s' % branch_match.group( 1 ) )
+		# Check if we are up to date.
+		up_to_date = re.search( u"Your branch is up to date with", out, re.MULTILINE )
+	else:
+		tag_match = re.search( u"^HEAD detached at (.+)$", out, re.MULTILINE )
+		print( 'Current tag: %s' % tag_match.group( 1 ) )
+		# Check if we are up to date.
+		up_to_date = re.search( u"nothing to commit", out, re.MULTILINE )
 
-	# Check if we are up to date.
-	up_to_date = re.search( u"Your branch is up to date with", out, re.MULTILINE )
 	if not skip_up_to_date and not up_to_date:
 		print( 'Repo not up to date with remote. Aborting. ' )
 		return False
-
 	# Check if there are unstaged commits.
 	unstaged = re.search( u"Changes not staged for commit:", out, re.MULTILINE )
 	if unstaged:
@@ -47,22 +55,33 @@ def check_if_clean( module, skip_up_to_date = False ):
 	return True
 
 def git_pull( module ):
-        print( '\033[1mPulling in  module %s \033[0m' % module )
-        path = os.path.join( ROOT_PATH, module )
-        out = run_command( "git pull", path )
+	print( 'Pulling in  module %s' % module )
+	path = os.path.join( ROOT_PATH, module )
+	out = run_command( "git pull", path )
 
 def get_branch_name( module ):
-    path = os.path.join( ROOT_PATH, module )
-    out = run_command( "git status -uno", path )
-    branch_match = re.search( u"^On branch (.+)$", out, re.MULTILINE )
-    return branch_match.group( 1 )
+	path = os.path.join( ROOT_PATH, module )
+	out = run_command( "git status -uno", path ).decode('ISO-8859-1')
+	branch_match = re.search( u"^On branch (.+)$", out, re.MULTILINE )
+	if branch_match is not None:
+		return branch_match.group( 1 )
+	else:
+		tag_match = re.search( u"^HEAD detached at (.+)$", out, re.MULTILINE )
+		return tag_match.group( 1 )
 
-
-def install():
-    branch_name = get_branch_name( '../mastodon' )
-    t = datetime.date.today()
-    cmd = "mvn clean install -Dscijava.app.directory=./Mastodon-%s-%s" % ( branch_name, t )
-    subprocess.call( cmd, cwd = ROOT_PATH, shell = True )
+def install( default_location = True ):
+	if not default_location:
+		branch_name = get_branch_name( '../mastodon' )
+		t = datetime.date.today()
+		target_dir = './Mastodon-%s-%s' % ( branch_name, t )
+		if not os.path.exists( target_dir ):
+			os.mkdir( target_dir )
+		print( 'Installing to %s' % target_dir )
+		cmd = "mvn clean install -Dscijava.app.directory=%s" % os.path.abspath( target_dir )
+	else:
+		print( 'Installing to default SciJava location' )
+		cmd = "mvn clean install"
+	subprocess.call( cmd, cwd = ROOT_PATH, shell = True )
 
 #-----------------------
 # MAIN
@@ -70,28 +89,27 @@ def install():
 
 def main():
 
-    modules = get_modules()
+	modules = get_modules()
 
-    # Check each module.
-    print( '\n----------------' )
-    print( 'Checking modules' )
-    for module in modules:
-        if not check_if_clean( module, module == '../mastodon-app' ):
-            return
+	# Check each module.
+	print( '\n----------------' )
+	print( 'Checking modules' )
+	for module in modules:
+		if not check_if_clean( module, module == '../mastodon-app' ):
+			return
 
-    # Pull each module.
-    print( '\n-------------------' )
-    print( 'Pulling from remote' )
-    for module in modules:
-        git_pull( module )
-    print( 'Done.' )
+	# Pull each module.
+	print( '\n-------------------' )
+	print( 'Pulling from remote' )
+	for module in modules:
+		git_pull( module )
+	print( 'Done.' )
 
 
-    print( '\n----------' )
-    print( 'Installing' )
-    install()
-    print( 'Done.' )
+	print( '\n----------' )
+	print( 'Installing' )
+	install()
+	print( 'Done.' )
 
 if __name__ == "__main__":
-    main()
-
+	main()

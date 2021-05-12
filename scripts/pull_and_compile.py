37ec40448d34df9df6b832b29acf3cd05a679157
Jean-Yves Tinevez <tinevez@pasteur.fr>
2021-05-12 17:49:44 +0200

    Improve the deployment script.
    - It now reads the desired version specified in the dependencies
    section of the pom, and check these versions before running the install.
    Super handy to cut releases.
    - Also, when deploying in a local folder, a second folder is created
    with just the mastodon jars instead of the whole dependency stack.

--- scripts/pull_and_compile.py
+++ scripts/pull_and_compile.py
@@ -4,6 +4,9 @@ import os
 import re
 import subprocess
 import datetime
+import sys
+import glob
+import shutil
 
 ROOT_PATH = '..'
 PATH_TO_POM_FILE = os.path.join( ROOT_PATH, 'pom.xml' )
@@ -24,6 +27,17 @@ def get_modules():
 		for matchNum, match in enumerate(matches, start=1):
 			modules.append( match.group(1) )
 	return modules
+
+def get_module_version( module ):
+	"""Reads the module version in the pom by looking at the dependencies
+	section."""
+	module_name = module[3:] # skip the ../
+	regex = ( u"\<" + module_name + "\.version\>(.+)\</" + module_name + "\.version\>")
+	with open( PATH_TO_POM_FILE, 'r' ) as pom_file:
+		matches = re.finditer(regex, pom_file.read(), re.MULTILINE)
+		for matchNum, match in enumerate(matches, start=1):
+			return match.group(1)
+	return ""
 
 def check_if_clean( module, skip_up_to_date = False ):
 	print( 'Checking module %s ' % module )
@@ -59,6 +73,17 @@ def git_pull( module ):
 	path = os.path.join( ROOT_PATH, module )
 	out = run_command( "git pull", path )
 
+def git_checkout_master( module ):
+	path = os.path.join( ROOT_PATH, module )
+	out = run_command( "git checkout master", path )
+
+def git_checkout_version( module, version ):
+	path = os.path.join( ROOT_PATH, module )
+	# Now checkout the desired version.
+	module_name = module[3:] # skip the ../
+	print( 'Checkout version %s of module %s' % ( version, module_name ) )
+	out = run_command( "git -c advice.detachedHead=false checkout %s-%s" % ( module_name, version ), path )
+
 def get_branch_name( module ):
 	path = os.path.join( ROOT_PATH, module )
 	out = run_command( "git status -uno", path ).decode('ISO-8859-1')
@@ -73,7 +98,10 @@ def install( default_location = True ):
 	if not default_location:
 		branch_name = get_branch_name( '../mastodon' )
 		t = datetime.date.today()
-		target_dir = './Mastodon-%s-%s' % ( branch_name, t )
+		if branch_name.lower().startswith( 'mastodon' ):
+			target_dir = './%s-%s-all' % ( branch_name, t )
+		else:
+			target_dir = './Mastodon-%s-%s-all' % ( branch_name, t )
 		if not os.path.exists( target_dir ):
 			os.mkdir( target_dir )
 		print( 'Installing to %s' % target_dir )
@@ -83,6 +111,19 @@ def install( default_location = True ):
 		cmd = "mvn clean install"
 	subprocess.call( cmd, cwd = ROOT_PATH, shell = True )
 
+	if not default_location:
+		subcopy_mastodon_jar( target_dir )
+
+def subcopy_mastodon_jar( target_dir ):
+	# Make a separate copy of just the mastodon jar if we have to.
+	copy_dir = target_dir[0:-4]
+	if not os.path.exists( copy_dir ):
+		os.mkdir( copy_dir )
+
+	for file in glob.glob( './' + target_dir + '/jars/mastodon-*.jar' ):
+		print( 'Copying %s to %s ' % ( file, copy_dir ) )
+		shutil.copy( file, copy_dir )
+
 #-----------------------
 # MAIN
 #-----------------------
@@ -90,6 +131,13 @@ def install( default_location = True ):
 def main():
 
 	modules = get_modules()
+
+	# Read the desired version.
+	print( '\n----------------' )
+	print( 'Version specified for modules:' )
+	for module in modules:
+		version = get_module_version( module )
+		print( ' %30s -> %s' % ( module, version ) )
 
 	# Check each module.
 	print( '\n----------------' )
@@ -103,13 +151,22 @@ def main():
 	print( 'Pulling from remote' )
 	for module in modules:
 		git_pull( module )
+		version = get_module_version( module )
+		git_checkout_version( module, version )
 	print( 'Done.' )
-
 
 	print( '\n----------' )
 	print( 'Installing' )
-	install()
+	install( True )
 	print( 'Done.' )
+
+	# Go back to master.
+	print( '\n----------' )
+	print( 'Switch back to master branches' )
+	for module in modules:
+		git_checkout_master( module )
+	print( 'Done.')
+
 
 if __name__ == "__main__":
 	main()
